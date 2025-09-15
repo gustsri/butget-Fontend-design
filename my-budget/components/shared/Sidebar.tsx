288c7a92b605f626fb297df02c278226415d324f
Bikeza7575 <158298546+Bikeza7575@users.noreply.github.com>
2025-09-15 15:56:26 +0700

    Disbursement

--- my-budget/components/shared/Sidebar.tsx
+++ my-budget/components/shared/Sidebar.tsx
@@ -48,7 +48,7 @@ const Sidebar: React.FC = () => {
           </Link>
         </li>
         <li>
-          <Link href="/planning/students" className="flex items-center px-3 py-2 rounded text-gray-700 hover:bg-gray-100">
+          <Link href="/planning/disbursement" className="flex items-center px-3 py-2 rounded text-gray-700 hover:bg-gray-100">
             <img src="/img/icon/document-signed.svg" alt="" className="w-5 h-5 mr-3" />
             การเบิกจ่าย
           </Link>
@@ -90,7 +90,7 @@ const Sidebar: React.FC = () => {
         </li>
 
         <li>
-          <Link href="/planning/students" className="flex items-center px-3 py-2 rounded text-gray-700 hover:bg-gray-100">
+          <Link href="#" className="flex items-center px-3 py-2 rounded text-gray-700 hover:bg-gray-100">
             <img src="/img/icon/person-add.svg" alt="" className="w-5 h-5 mr-3" />
             การอนุมัติการเบิกจ่าย
           </Link>
